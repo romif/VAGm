d992d35c421cd7964bce4ddfe588d48e4dbecd1c
Admin <Admin@10.4.156.238>
2014-05-01 14:49:07 +0300

    fix resource bug

--- src/main/java/com/vagm/vagmdroid/activities/MainActivity.java
+++ src/main/java/com/vagm/vagmdroid/activities/MainActivity.java
@@ -23,7 +23,6 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
